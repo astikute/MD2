9c601dcfc813acbdb320d07966f8da9e3e030ca0
astikute <agnese.stikute@gmail.com>
2018-05-15 18:58:34 +0300

    MD2
    Pievienots while cikls izvelne

--- MD2/MD2/Program.cs
+++ MD2/MD2/Program.cs
@@ -10,31 +10,47 @@ namespace MD2
     {
         static void Main(string[] args)
         {
-            //Uzdevums_1
-            Console.WriteLine("1.uzdevums");
-            Console.Write("Ievadiet skaitli: ");
-            int sk = Convert.ToInt32(Console.ReadLine());
+            int izvele = 1;
 
-            for (int i = 1; i < 11; i++)
+            while (izvele != 0)
             {
-                int sk_rez = sk * i;
-                Console.WriteLine("{0} * {1} = {2}", sk, i, sk_rez);
+                Console.WriteLine("1. Izvadit reizrekina tabulu");
+                Console.WriteLine("2. Skaitit no 10 uz leju");
+                Console.WriteLine("3. Izvadit zvaigznites");
+                Console.WriteLine("4. Faktorials");
+                Console.WriteLine("0. Izeja");
+
+                string input = Console.ReadLine();
+                izvele = Convert.ToInt32(input);
+
+                switch(izvele)
+                {
+                    case 1:
+                        Console.WriteLine();
+                        Console.WriteLine("1.uzdevums");
+                        Tabula();
+                        break;
+                    case 2:
+                        Console.WriteLine();
+                        Console.WriteLine("2.uzdevums");
+                        Zvaigznites();
+                        break;
+                    case 3:
+                        Console.WriteLine();
+                        Console.WriteLine("3.uzdevums");
+                        UzLeju();
+                        break;
+                    case 4:
+                        Faktorials();
+                        break;
+                    case 0:
+                        izvele = 0;
+                        break;
+                    default:
+                        Console.WriteLine("Neeksiste!");
+                        break;
+                }
             }
-
-            //Uzdevums_2
-            Console.WriteLine();
-            Console.WriteLine("2.uzdevums");
-            Zvaigznites();
-
-            //Uzdevums_3
-            Console.WriteLine();
-            Console.WriteLine("3.uzdevums");
-            for (int i = 10; i > 0; i--)
-            {
-                Console.Write(i + " ");
-            }
-
-            Console.Read();
         }
 
         static void Zvaigznites()
@@ -46,6 +62,53 @@ namespace MD2
                 virkne = virkne + "*";
                 Console.WriteLine(virkne);
             }
+            Console.WriteLine();
+        }
+
+        static void Tabula()
+        {
+            Console.Write("Ievadiet skaitli: ");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            int sk_rez;
+
+            for (int i = 1; i < 11; i++)
+            {
+                sk_rez = sk * i;
+                Console.WriteLine("{0} * {1} = {2}", sk, i, sk_rez);
+            }
+            Console.WriteLine();
+         }
+
+        static void UzLeju()
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Faktorials()
+        {
+            Console.Write("Ievadiet skaitli: ");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            int rez = 0;
+
+            if (sk == 0) // Skaitla 0! = 1
+            {
+                Console.Write("0! = 1");
+            }
+            else
+            {
+                for (int i = 1; i <= sk; i++) // Parejiem skaitliem, sakot no 1
+                {
+                    int vecaRez = rez;
+                    rez = rez + i;
+                    Console.WriteLine(vecaRez + "+" + i + "=" + rez);
+                }
+                Console.Write("{0}! = {1}", sk, rez);
+            }
+            Console.WriteLine();
         }
     }
 }
